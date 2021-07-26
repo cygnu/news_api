9272e90aff897df9c5578ab5ef2f0c793bab039f
cygnu <crucia8@icloud.com>
2021-07-27 03:29:38 +0900

    error: The named parameter 'article' is required, but there's no corresponding argument

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/article_model.dart';
+import 'package:news_app/screens/details_screen.dart';
 import 'package:news_app/screens/overview_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -19,6 +20,7 @@ class NewsApp extends StatelessWidget {
         routes: {
           '/': (context) => OverviewScreen(),
           OverviewScreen.route: (context) => OverviewScreen(),
+          DetailsScreen.route: (context) => DetailsScreen(),
         },
       ),
     );
