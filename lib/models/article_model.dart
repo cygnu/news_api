6f8affc2ef33e68559e0d152049c67533fb4fb96
cygnu <crucia8@icloud.com>
2021-07-26 17:42:16 +0900

    feat: Implement the function 'getArticles' in models/article_model.dart

---
+++ lib/models/article_model.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import 'article.dart';
+
+const NewsAPIURL = 'https://newsapi.org/v2/top-headlines?country=jp';
+const apiKey = '657ad8xxxxxx';
+
+class ArticleModel extends ChangeNotifier {
+  List<Article> articlesList = [];
+
+  Future<List<Article>> getArticles() async {
+    http.Response response = await http.get('$NewsAPIURL&apiKey=$apiKey');
+
+    if (response.statusCode != 200) {
+      print(response.statusCode);
+    }
+
+    Map<String, dynamic> articlesData = jsonDecode(response.body);
+    for (var article in articlesData['articles']) {
+      articlesList.add(Article.fromJson(article));
+    }
+
+    return articlesList;
+  }
+}
+
