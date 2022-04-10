a1575452d3bd9e0053ee93023df6f835590f6650
cygnu <crucia8@icloud.com>
2022-04-11 03:36:13 +0900

    feat: Update components/news_list_view.dart

--- lib/components/news_list_view.dart
+++ lib/components/news_list_view.dart
@@ -1,31 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:news_api/components/news_item.dart';
-import 'package:news_api/data/models/news_article.dart';
-import 'package:news_api/screens/views/main_view_model.dart';
+import 'package:news_api/data/view_model/main_view_model.dart';
 import 'package:provider/provider.dart';
 
 class NewsListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final articleModel = Provider.of<MainViewModel>(context);
-    articleModel.getNews("us", dotenv.env["API_KEY"]);
+    final articleModel = MainViewModel();
+    articleModel.fetchNews("us", dotenv.env["API_KEY"]);
 
-    return Consumer<MainViewModel>(
-      builder: (BuildContext context, model, Widget? child) {
-        List<NewsArticle> articles = [];
-
-        for (int i = 0; i < model.news.length; i++) {
-          articles.addAll(model.news[i].articles);
-        }
-
-        return ListView.builder(
-          itemCount: articles.length,
-          itemBuilder: (BuildContext context, int index) => NewsItem(
-            article: articles[index],
-          ),
-        );
-      },
+    return ChangeNotifierProvider(
+      create: (context) => articleModel,
+      child: Consumer<MainViewModel>(
+        builder: (BuildContext context, MainViewModel articleModel, _) {
+          return ListView.builder(
+            itemCount: articleModel.news.length,
+            itemBuilder: (BuildContext context, int index) => NewsItem(
+              article: articleModel.news[index].articles[index],
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:news_api/screens/details_screen.dart';
 import 'package:news_api/screens/overview_screen.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
   runApp(
-    ProviderScope(
-      child: NewsApi(),
-    ),
+    NewsApi(),
   );
 }
 
