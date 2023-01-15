b82f8e8138fa79a3d2a61774e65284861f247f4d
cygnu <crucia8@icloud.com>
2023-01-15 20:31:19 +0900

    fix: Add provider scope to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:news_api/screens/details_screen.dart';
 import 'package:news_api/screens/overview_screen.dart';
-import 'package:news_api/screens/views/main_view_model.dart';
-import 'package:provider/provider.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
-  runApp(newsApi());
+  runApp(
+    ProviderScope(child: NewsApi()),
+  );
 }
 
-class newsApi extends StatelessWidget {
+class NewsApi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<MainViewModel>(
-          create: (_) => MainViewModel(),
-        ),
-      ],
-      child: MaterialApp(
-        initialRoute: '/',
-        routes: {
-          '/': (context) => OverviewScreen(),
-          OverviewScreen.route: (context) => OverviewScreen(),
-          DetailsScreen.route: (context) => DetailsScreen(),
-        },
-      ),
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => OverviewScreen(),
+        OverviewScreen.route: (context) => OverviewScreen(),
+        DetailsScreen.route: (context) => DetailsScreen(),
+      },
     );
   }
 }
