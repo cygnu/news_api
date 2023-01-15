3f2c86cf28b4338c2f96e09b30958cb1ecc1822d
cygnu <crucia8@icloud.com>
2023-01-15 21:46:35 +0900

    fix: Use StateNotifier class in screens/views/main_view_model.dart

--- lib/screens/views/main_view_model.dart
+++ lib/screens/views/main_view_model.dart
@@ -1,16 +1,11 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:news_api/data/api_client.dart';
 import 'package:news_api/data/models/news_info.dart';
 
-class MainViewModel extends ChangeNotifier {
-  late String country;
-  late String apiKey;
-
-  List<NewsInfo> _news = [];
-  List<NewsInfo> get news => _news;
+class MainViewNotifier extends StateNotifier<List<NewsInfo>> {
+  MainViewNotifier() : super([]);
 
   Future<void> getNews(country, apiKey) async {
-    _news = await getHeadlineNews(country, apiKey);
-    notifyListeners();
+    await getHeadlineNews(country, apiKey);
   }
 }
