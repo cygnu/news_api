976c3d4763997b586048e1f589a818f508008890
cygnu <crucia8@icloud.com>
2023-01-15 23:26:22 +0900

    fix: Changed to reference from hooks_riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:news_api/screens/details_screen.dart';
 import 'package:news_api/screens/overview_screen.dart';
 
--- lib/screens/views/main_view_model.dart
+++ lib/screens/views/main_view_model.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:news_api/data/api_client.dart';
 import 'package:news_api/data/models/news_info.dart';
 
