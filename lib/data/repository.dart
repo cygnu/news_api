4b2cb354cc719fd9090cbf7282b2cc56f56f8699
cygnu <crucia8@icloud.com>
2023-01-17 22:57:31 +0900

    feat: #1 Create ApiRepository class

---
+++ lib/data/repository.dart
@@ -0,0 +1,17 @@
+import 'package:news_api/data/api_client.dart';
+import 'package:news_api/data/models/news_info.dart';
+
+abstract class ApiRepository {
+  Future<List<NewsInfo>> getNews(String country, String apiKey);
+}
+
+class ApiRepositoryImpl implements ApiRepository {
+  ApiRepositoryImpl(this._apiClient);
+
+  final ApiClient _apiClient;
+
+  @override
+  Future<List<NewsInfo>> getNews(String country, String apiKey) async {
+    return await _apiClient.getHeadlineNews(country, apiKey);
+  }
+}
