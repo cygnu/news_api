66ca0c2e8e90d40df3322d8389a47c40fa237e54
cygnu <crucia8@icloud.com>
2023-01-17 22:56:27 +0900

    fix: #1 Create ApiClient class to be called from various repository

--- lib/data/api_client.dart
+++ lib/data/api_client.dart
@@ -3,13 +3,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:news_api/data/models/news_info.dart';
 
-final baseUrl = "https://newsapi.org/v2/top-headlines";
+class ApiClient {
+  final baseUrl = "https://newsapi.org/v2/top-headlines";
 
-Future<List<NewsInfo>> getHeadlineNews(String country, String apiKey) async {
-  final response =
-      await http.get(Uri.https("baseUrl?country=$country&apiKey=$apiKey"));
+  Future<List<NewsInfo>> getHeadlineNews(String country, String apiKey) async {
+    final response =
+        await http.get(Uri.https("baseUrl?country=$country&apiKey=$apiKey"));
 
-  List<NewsInfo> info =
-      NewsInfo.fromJson(jsonDecode(response.body)) as List<NewsInfo>;
-  return info;
+    List<NewsInfo> res =
+        NewsInfo.fromJson(jsonDecode(response.body)) as List<NewsInfo>;
+    return res;
+  }
 }
