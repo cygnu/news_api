4bd8b9d16ffa7a10fb0e387f1c1673d1c3a73cc9
cygnu <crucia8@icloud.com>
2023-01-21 07:19:11 +0900

    feat: #1 Asynchronous communication loading was performed by using AsyncValue in screens/views/main_view_model.dart

--- lib/screens/views/main_view_model.dart
+++ lib/screens/views/main_view_model.dart
@@ -1,11 +1,38 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:news_api/data/api_client.dart';
 import 'package:news_api/data/models/news_info.dart';
+import 'package:news_api/data/repository.dart';
 
-class MainViewNotifier extends StateNotifier<List<NewsInfo>> {
-  MainViewNotifier() : super([]);
+final mainViewModelProvider =
+    StateNotifierProvider<MainViewModel, AsyncValue<List<NewsInfo>>>(
+  (ref) => MainViewModel(
+    ApiRepositoryImpl(),
+  ),
+);
 
-  Future<void> getNews(country, apiKey) async {
-    await getHeadlineNews(country, apiKey);
+class MainViewModel extends StateNotifier<AsyncValue<List<NewsInfo>>> {
+  MainViewModel(this._apiRepositoryImpl)
+      : super(const AsyncLoading<List<NewsInfo>>()) {
+    getNews();
+  }
+
+  final ApiRepositoryImpl _apiRepositoryImpl;
+
+  Future<void> getNews({bool isLoadMore = false}) async {
+    state = await AsyncValue.guard(() async {
+      final data =
+          await _apiRepositoryImpl.getNews("us", dotenv.env["API_KEY"]!);
+      return [if (isLoadMore) ...state.value ?? [], ...data];
+    });
+  }
+
+  void loadMore() {
+    if (state == const AsyncLoading<List<NewsInfo>>().copyWithPrevious(state)) {
+      return;
+    }
+
+    state = const AsyncLoading<List<NewsInfo>>().copyWithPrevious(state);
+
+    getNews(isLoadMore: true);
   }
 }
