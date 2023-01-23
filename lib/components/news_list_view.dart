53ab848d7e116e6d8e4a53473c43207d1e63b3f2
cygnu <crucia8@icloud.com>
2023-01-23 23:56:44 +0900

    feat: #1 #3 The acquired data is displayed on the screen. Also, begin implementation of infinite scrolling

--- lib/components/news_list_view.dart
+++ lib/components/news_list_view.dart
@@ -1,38 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:news_api/components/news_item.dart';
-import 'package:news_api/data/models/news_article.dart';
 import 'package:news_api/data/models/news_info.dart';
 import 'package:news_api/screens/views/main_view_model.dart';
-
-final viewProvider =
-    StateNotifierProvider<MainViewNotifier, List<NewsInfo>>((ref) {
-  return MainViewNotifier();
-});
 
 class NewsListView extends HookConsumerWidget {
   const NewsListView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final articleNotifier = ref.watch(viewProvider.notifier);
-    articleNotifier.getNews("us", dotenv.env["API_KEY"]);
+    final AsyncValue<List<NewsInfo>> asyncValue =
+        ref.watch(mainViewModelProvider);
 
-    return Consumer(
-      builder: (BuildContext context, model, _) {
-        List<NewsArticle> articles = [];
-
-        for (int i = 0; i < articles.length; i++) {
-          articles.addAll(articles);
-        }
-
-        return ListView.builder(
-          itemCount: articles.length,
-          itemBuilder: (BuildContext context, int index) => NewsItem(
-            article: articles[index],
-          ),
-        );
-      },
+    return Scrollbar(
+      child: CustomScrollView(),
     );
   }
 }
