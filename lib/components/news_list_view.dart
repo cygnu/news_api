61e332d64df363b4fcad42786f6f2ee224098c12
cygnu <crucia8@icloud.com>
2023-01-25 09:30:55 +0900

    feat: #1 #3 Create the general framework of the screen

--- lib/components/news_list_view.dart
+++ lib/components/news_list_view.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:news_api/components/news_item.dart';
 import 'package:news_api/data/models/news_info.dart';
 import 'package:news_api/screens/views/main_view_model.dart';
 
@@ -11,7 +13,37 @@ class NewsListView extends HookConsumerWidget {
         ref.watch(mainViewModelProvider);
 
     return Scrollbar(
-      child: CustomScrollView(),
+      child: CustomScrollView(
+        slivers: [
+          asyncValue.when(
+            data: (List<NewsInfo> data) {
+              return NewsItem(article: data);
+            },
+            error: (Object error, StackTrace stackTrace) {
+              return SliverPadding(
+                padding: EdgeInsets.all(16.0),
+                sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return Container(
+                        child: Text('Error.'),
+                      );
+                    },
+                  ),
+                ),
+              );
+            },
+            loading: () {
+              return SliverPadding(
+                padding: EdgeInsets.all(16.0),
+                sliver: Center(
+                  child: CupertinoActivityIndicator(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
