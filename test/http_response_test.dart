ba59375c13f9330ce717d9ea6db5fdd3c2001e11
cygnu <crucia8@icloud.com>
2023-01-28 13:56:16 +0900

    test: I set up a mock server

--- test/http_response_test.dart
+++ test/http_response_test.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mock_web_server/mock_web_server.dart';
 import 'package:news_api/data/api_client.dart';
-import 'package:news_api/data/repository.dart';
 
 void main() {
-  late ApiRepositoryImpl _apiRepository;
-  final ApiClient _apiClient;
+  final _server = MockWebServer(port: 8081);
+  ApiClient _apiClient;
 
   setUp(() {
-    _apiRepository = ApiRepositoryImpl(apiClient: _apiClient)
-        .getNews("us", dotenv.env["API_KEY"]!) as ApiRepositoryImpl;
+    // Http通信成功時
+    _apiClient =
+        ApiClient().getHeadlineNews("us", dotenv.env["API_KEY"]!) as ApiClient;
   });
+
+  tearDown(_server.shutdown);
 }
