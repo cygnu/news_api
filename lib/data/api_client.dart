ad27fa2e9bb84644c94b3b6e430699db4bdf27df
cygnu <crucia8@icloud.com>
2023-02-04 18:02:21 +0900

    fix: Rebuild response of ApiClient class

--- lib/data/api_client.dart
+++ lib/data/api_client.dart
@@ -1,22 +1,23 @@
 import 'dart:convert';
 
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_api/data/models/news_info.dart';
 
-final apiClientProvider = Provider.autoDispose(
-  (_) => ApiClient(),
-);
-
 class ApiClient {
-  final baseUrl = "https://newsapi.org/v2/top-headlines";
+  final baseUrl = 'https://newsapi.org/v2/top-headlines';
 
   Future<List<NewsInfo>> getHeadlineNews(String country, String apiKey) async {
     final response =
-        await http.get(Uri.https("$baseUrl?country=$country&apiKey=$apiKey"));
+        await http.get(Uri.https('$baseUrl?country=$country&apiKey=$apiKey'));
 
-    List<NewsInfo> res =
-        NewsInfo.fromJson(jsonDecode(response.body)) as List<NewsInfo>;
-    return res;
+    if (response.statusCode == 200) {
+      final decodedJson = jsonDecode(response.body) as List<NewsInfo>;
+      return decodedJson
+          .map((e) => NewsInfo.fromJson(e as Map<String, NewsInfo>))
+          .toList();
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+      return [];
+    }
   }
 }
